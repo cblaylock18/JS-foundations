6614014b02ad9a4111223aa3202db5792acb83c5
Christopher Blaylock <160085222+cblaylock18@users.noreply.github.com>
2024-04-01 20:20:57 -0400

    Add DOM manipulation exercises during active reading on ODIN

---
+++ javascript.js
@@ -0,0 +1,39 @@
+const container = document.querySelector("#container");
+
+const content = document.createElement("div");
+content.classList.add("content");
+content.textContent = "This is the glorious text-content!";
+
+container.appendChild(content);
+
+const redContent = document.createElement("p");
+redContent.style.color = "red";
+redContent.textContent = "Hey I'm red!";
+container.appendChild(redContent);
+
+const blueH3 = document.createElement("h3");
+blueH3.style.color = "blue";
+blueH3.textContent = "I'm a blue h3!";
+container.appendChild(blueH3);
+
+const pinkDiv = document.createElement("div");
+pinkDiv.style.cssText =
+    "background: pink; border-style: solid; border-width: 2px; border-color: black";
+
+const normalH1 = document.createElement("h1");
+normalH1.textContent = "I'm in a div";
+pinkDiv.appendChild(normalH1);
+
+const divP = document.createElement("p");
+divP.textContent = "ME TOO!";
+pinkDiv.appendChild(divP);
+
+container.appendChild(pinkDiv);
+
+const btn1 = document.querySelector("#btn-1");
+btn1.onclick = () => alert("Hello World");
+
+const btn2 = document.querySelector("#btn-2");
+btn2.addEventListener("click", function (e) {
+    e.target.style.background = "blue";
+});
